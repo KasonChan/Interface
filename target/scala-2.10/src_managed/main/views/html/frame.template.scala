1b0e55d58ee00cffb0567b1882d917c2324df724
KasonChan <kasonl.chan@gmail.com>
2014-07-07 20:10:38 -0500

    Edited frame

--- target/scala-2.10/src_managed/main/views/html/frame.template.scala
+++ target/scala-2.10/src_managed/main/views/html/frame.template.scala
@@ -38,20 +38,20 @@ Seq[Any](format.raw/*1.32*/("""
 <body>
   <!-- Reactive ribbon -->
   <a href="http://www.reactivemanifesto.org/"> <img style="border: 0; position: fixed; right: 0; top:0; z-index: 9000" src="//d379ifj7s9wntv.cloudfront.net/reactivemanifesto/images/ribbons/we-are-reactive-blue-right.png"> </a>
-  <!-- Header -->
-  <div id="header">
-    <div class="container">
-      <h1>
-        <a href=""""),_display_(Seq[Any](/*24.19*/routes/*24.25*/.Application.index)),format.raw/*24.43*/("""">"""),_display_(Seq[Any](/*24.46*/title)),format.raw/*24.51*/("""</a>
-      </h1>
+  <div class="application_wrapper">
+    <!-- Header -->
+    <div id="header">
+      <div class="container">
+        <h1>
+          <a href=""""),_display_(Seq[Any](/*25.21*/routes/*25.27*/.Application.index)),format.raw/*25.45*/("""">"""),_display_(Seq[Any](/*25.48*/title)),format.raw/*25.53*/("""</a>
+        </h1>
+      </div>
     </div>
-  </div>
-  <!-- Output page content block -->
-  <div class="content">
+    <!-- Output page content block -->
     """),_display_(Seq[Any](/*30.6*/content)),format.raw/*30.13*/("""
+    <!-- Footer -->
+    """),_display_(Seq[Any](/*32.6*/footer())),format.raw/*32.14*/("""
   </div>
-  <!-- Footer -->
-  """),_display_(Seq[Any](/*33.4*/footer())),format.raw/*33.12*/("""
 </body>
 </html>
 """))}
@@ -66,11 +66,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Mon Jul 07 15:22:02 CDT 2014
+                    DATE: Mon Jul 07 19:41:55 CDT 2014
                     SOURCE: /home/ka-son/Documents/Interface/app/views/frame.scala.html
-                    HASH: e567a9a9d7b24a0b14cef19823a93bc3877c7443
-                    MATRIX: 561->1|685->31|763->74|789->79|1088->342|1103->348|1160->383|1270->457|1285->463|1339->495|1394->514|1409->520|1476->565|1911->964|1926->970|1966->988|2005->991|2032->996|2170->1099|2199->1106|2265->1137|2295->1145
-                    LINES: 19->1|22->1|28->7|28->7|33->12|33->12|33->12|35->14|35->14|35->14|36->15|36->15|36->15|45->24|45->24|45->24|45->24|45->24|51->30|51->30|54->33|54->33
+                    HASH: 60e9e4519a6fa64629c104593f544cb00d8c01bf
+                    MATRIX: 561->1|685->31|763->74|789->79|1088->342|1103->348|1160->383|1270->457|1285->463|1339->495|1394->514|1409->520|1476->565|1957->1010|1972->1016|2012->1034|2051->1037|2078->1042|2200->1129|2229->1136|2290->1162|2320->1170
+                    LINES: 19->1|22->1|28->7|28->7|33->12|33->12|33->12|35->14|35->14|35->14|36->15|36->15|36->15|46->25|46->25|46->25|46->25|46->25|51->30|51->30|53->32|53->32
                     -- GENERATED --
                 */
             
