b5a01a07568a548142167f428e23492b623bb9f7
KasonChan <kasonl.chan@gmail.com>
2014-07-19 17:04:03 -0500

    Working on submission.

--- submissions/kasonchan/submission1/compositions/A.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-unzip scala.zip -d scala && \
-./scala/scala/bin/scalac A.scala && \
-./scala/scala/bin/scala A inputA outputA && \
-rm -r scala *.class
--- submissions/kasonchan/submission1/submission.sh
+++
@@ -1,4 +0,0 @@
-echo "*****START ./interface.sh"
-cd $1
-./interface.sh $2 $3 $4 $5
-echo "*****END ./interface.sh
